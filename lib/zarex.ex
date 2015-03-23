ebb85bfc8e39c06cc548dbdc7230890e06c7209d
Richard Nyström <ricn@rny.io>
2015-03-24 00:27:05 +0100

    Got some tests passing

--- lib/zarex.ex
+++ lib/zarex.ex
@@ -1,2 +1,7 @@
 defmodule Zarex do
+  def sanitize(name, padding \\ 0) do
+    String.strip(name)
+    |> String.replace(~r/[[:space:]]+/u, " ")
+    |> String.slice(0, 255 - padding)
+  end
 end
--- test/zarex_test.exs
+++ test/zarex_test.exs
@@ -1,7 +1,17 @@
 defmodule ZarexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "normalization" do
+    Enum.each(["a", " a", "a ", " a ", "a    \n"],
+    fn(name) -> assert Zarex.sanitize(name) == "a" end)
+
+    Enum.each(["x x", "x  x", "x   x", "x\tx", "x\r\nx"],
+    fn(name) -> assert Zarex.sanitize(name) == "x x" end)
+  end
+
+  test "truncation" do
+    name = String.duplicate("A", 400)
+    assert String.length(Zarex.sanitize(name)) == 255
+    assert String.length(Zarex.sanitize(name, 10)) == 245
   end
 end
