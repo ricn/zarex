ff610a0fd4aaa55625b556968475570c6b5286ae
Richard Nyström <ricn@rny.io>
2015-03-24 11:25:53 +0100

    We don't need a result

--- lib/zarex.ex
+++ lib/zarex.ex
@@ -1,6 +1,6 @@
 defmodule Zarex do
   def sanitize(name, padding \\ 0) do
-    result = String.strip(name)
+    String.strip(name)
     |> String.replace(~r/[[:space:]]+/u, " ") #normalize
     |> String.slice(0, 255 - padding) #padding
     |> String.replace(~r/[\x00-\x1F\/\\:\*\?\"<>\|]/u, "") #sanitize
@@ -17,7 +17,8 @@ defmodule Zarex do
   end
 
   defp filter_windows_reserved_names(str) do
-    wrn = ~w(CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9 LPT1 LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9)
+    wrn = ~w(CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9 LPT1
+    LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9)
     case Enum.member?(wrn, String.upcase(str)) do
       true  -> "file"
       false -> str
