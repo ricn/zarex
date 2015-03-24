387245a9de30e44ade442988a57ad35dac9d6e40
Richard Nyström <ricn@rny.io>
2015-03-24 11:33:31 +0100

    Refactoring

--- lib/zarex.ex
+++ lib/zarex.ex
@@ -4,31 +4,22 @@ defmodule Zarex do
     |> String.replace(~r/[[:space:]]+/u, " ") #normalize
     |> String.slice(0, 255 - padding) #padding
     |> String.replace(~r/[\x00-\x1F\/\\:\*\?\"<>\|]/u, "") #sanitize
-    |> fallback_filename
     |> filter_windows_reserved_names
     |> filter_dots
+    |> filename_fallback
   end
 
-  defp fallback_filename(str) do
-    case String.length(str) do
-      0 -> "file"
-      _ -> str
-    end
+  defp filename_fallback(name) do
+    if String.length(name) == 0, do: "file", else: name
   end
 
-  defp filter_windows_reserved_names(str) do
+  defp filter_windows_reserved_names(name) do
     wrn = ~w(CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9 LPT1
     LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9)
-    case Enum.member?(wrn, String.upcase(str)) do
-      true  -> "file"
-      false -> str
-    end
+    if Enum.member?(wrn, String.upcase(name)), do: "file", else: name
   end
 
-  defp filter_dots(str) do
-    case String.starts_with?(str, ".") do
-      true  -> "file#{str}"
-      false -> str
-    end
+  defp filter_dots(name) do
+    if String.starts_with?(name, "."), do: "file#{name}", else: name
   end
 end
