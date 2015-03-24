de4b1019e622e1d5443182cf8dcba4e28821f0c3
Richard Nyström <ricn@rny.io>
2015-03-24 19:06:50 +0100

    Added travis CI. Closes #2

--- mix.exs
+++ mix.exs
@@ -5,26 +5,29 @@ defmodule Zarex.Mixfile do
     [app: :zarex,
      version: "0.0.1",
      elixir: "~> 1.0",
+     description: description,
+     package: package
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp description
+    "Filename sanitization for Elixir"
+  end
+
   defp deps do
-    []
+    [{:inch_ex, only: :docs},
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.7", only: :dev}]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     contributors: ["Richard Nyström"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/ricn/zarex", "Docs" => "http://hexdocs.pm/zarex"}]
   end
 end
