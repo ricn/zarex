bd5778fd5ba55fea4e552c73c036498011a44735
Richard Nyström <ricn@rny.io>
2017-07-25 13:03:12 +0200

    Version bump.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Zarex.Mixfile do
 
   def project do
     [app: :zarex,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.0",
      description: description(),
      package: package(),
